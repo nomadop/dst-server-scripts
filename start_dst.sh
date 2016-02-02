f2268f20b28d6d68552304ed04d516d06c76c2a7
nomadop <nomadop@gmail.com>
2016-02-02 21:32:12 +0800

    add start script for cave server

--- start_dst.sh
+++ start_dst.sh
@@ -2,4 +2,7 @@
 cd /root/steamapps/DST/bin
 
 /usr/bin/screen -d -m -S "DST_Overworld" /bin/sh -c './dontstarve_dedicated_server_nullrenderer -conf_dir DST_Overworld -skip_update_server_mods -console'
-echo "Starting DST Server..."
+echo "Starting DST Overworld Server..."
+
+/usr/bin/screen -d -m -S "DST_Caves" /bin/sh -c './dontstarve_dedicated_server_nullrenderer -conf_dir DST_Caves -skip_update_server_mods -console'
+echo "Starting DST Caves Server..."
