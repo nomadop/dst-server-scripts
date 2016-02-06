c8471b6199d5ca6d6cf77036c71a157711f923f5
nomadop <nomadop@gmail.com>
2016-02-06 10:35:39 +0800

    remove saves for this repo

--- backup_aliyun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp -rf /root/.klei/DST_Overworld/save ./saves/aliyun/DST_Overworld/
-echo "Backup DST Overworld Save."
-
-cp -rf /root/.klei/DST_Caves/save ./saves/aliyun/DST_Caves/
-echo "Backup DST Caves Save."
